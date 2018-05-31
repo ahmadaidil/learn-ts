8e656f6eaeab10d8b943bdb385321434962323d6
Ahmad Aidil <aidil@kofera.com>
2018-05-31 14:55:01 +0700

    added inheritance

---
+++ inheritance.ts
@@ -0,0 +1,35 @@
+class Human {
+  sex: string;
+  constructor(sex: string) {
+    this.sex = sex;
+  }
+  identify() {
+    return `My sex is ${this.sex}`;
+  }
+}
+
+class Woman extends Human {
+  constructor(sex: string = 'woman') {
+    super(sex);
+  }
+  cooking() {
+    return 'woman is cooking ...';
+  }
+}
+
+class Man extends Human {
+  constructor(sex: string = 'man') {
+    super(sex);
+  }
+  playing() {
+    return 'man is playing ...';
+  }
+}
+
+const woman = new Woman();
+const man = new Man();
+
+console.log(woman.identify());
+console.log(man.identify());
+console.log(woman.cooking());
+console.log(man.playing());
